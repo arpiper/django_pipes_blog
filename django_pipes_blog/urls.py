cc46bf3735c74c6c5e66e59b622d8215a5c776a3
Andrew Piper <pipera@uw.edu>
2018-01-29 16:29:01 -0800

    working on views

--- django_pipes_blog/urls.py
+++ django_pipes_blog/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url 
-from apps.django_pipes_blog import views
+from django.urls import path, re_path
+from . import views
 
 app_name = 'django_pipes_blog'
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/<str:title>/', views.SinglePostView),
+    re_path('<slug:slug>', views.SinglePostView)
 ]
--- django_pipes_blog/views.py
+++ django_pipes_blog/views.py
@@ -10,10 +10,29 @@ class IndexView(ListView):
 
     def get_queryset(self, *args, **kwargs):
         posts = Post.objects.filter(
-            user=self.request.user, 
             published=True
         ).order_by(
             '-date_published'
         )
         return posts
         
+
+class SinglePostView(DetailView):
+    model = Post
+    template = 'django_pipes_blog/post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, *args, **kwargs):
+        if 'year' in kwargs.keys():
+            year = kwargs['year'] 
+            month = kwargs['month']
+            day = kwargs['day']
+            title = kwargs['title']
+            post = Post.objects.filter(
+                date_published__year=year,
+                date_published__month=month,
+                date_published__day=day,
+                title=title
+            )
+            if len(post) === 1:
+                return post

5c2e24ae804f4d9c351283efd3f509fa1bc3ec8d
Andrew Piper <pipera@uw.edu>
2018-02-01 23:41:23 -0800

    regex hyphen for post re_path

--- django_pipes_blog/urls.py
+++ django_pipes_blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'django_pipes_blog'
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
-    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w]+)/$',
+    re_path(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[\w\-]+)/$',
         views.SinglePostView.as_view(),
         name='post'
     ),

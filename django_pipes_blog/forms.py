818f0937e863269b97e7c7e8606d001681b48f94
Andrew Piper <pipera@uw.edu>
2018-02-05 14:17:44 -0800

    changed the textarea widgets

--- django_pipes_blog/forms.py
+++ django_pipes_blog/forms.py
@@ -1,4 +1,5 @@
-from django.forms import ModelForm, inlineformset_factory
+from django.forms import ModelForm, inlineformset_factory, Textarea
+from django.utils.translation import gettext_lazy
 
 from .models import Post, TextBlock, PostImage
 
@@ -6,11 +7,20 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'published', 'tags']
+        widgets = {
+            'tags': Textarea(attrs={'rows': 3}),
+        }
+        help_texts = {
+            'tags': gettext_lazy('Space seperated list of tags relevant to the post.'),
+        }
 
 
 class TextBlockForm(ModelForm):
     class Meta:
         model = TextBlock
         fields = ['block_title', 'text']
+        widgets = {
+            'text': Textarea(attrs={'rows': 20}),
+        }
 
 TextBlockFormSet = inlineformset_factory(Post, TextBlock, form=TextBlockForm, extra=1)

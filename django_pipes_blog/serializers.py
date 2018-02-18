998981683fe63114306a956c7e27426eb427b0a9
Andrew Piper <pipera@uw.edu>
2018-02-17 22:06:56 -0800

    fixed url lookup issue

--- django_pipes_blog/serializers.py
+++ django_pipes_blog/serializers.py
@@ -4,12 +4,14 @@ from rest_framework import serializers
 from .models import Post, PostImage
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
-    images = serializers.HyperlinkedRelatedField(view_name='postimage-detail', read_only=True)
+    #images = serializers.HyperlinkedRelatedField(view_name='postimage-detail', read_only=True)
     user = serializers.ReadOnlyField(source='user.username')
-    url = serializers.HyperlinkedIdentityField(view_name='post-detail', lookup_field='pk')
+    url = serializers.HyperlinkedIdentityField(view_name='django_pipes_blog:post-detail', lookup_field='pk')
     class Meta:
         model = Post
-        fields = ('url', 'user', 'tags', 'title', 'text', 'published', 'date_published', 'date_created', 'images')
+        fields = ('url', 'user', 'tags', 'title', 'text', 'published', 
+            'date_published', 'date_created', 'postimage_set' 
+        )
 
 
 class PostImageSerializer(serializers.HyperlinkedModelSerializer):
@@ -17,9 +19,6 @@ class PostImageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = PostImage
         fields = ('url', 'name', 'image', 'post')
-        extra_kwargs = {
-            'post': {'lookup_field': 'slug'}
-        }
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
